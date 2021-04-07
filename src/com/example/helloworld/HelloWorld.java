2b9ada6f7ba4827b00a86648ff94c88bdcf81055
Julianne <julianatuhwera55@gmail.com>
2021-04-07 10:23:11 +0300

    added my javacode

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Happy Birthday Babe!\n" +
+                "You are my only inspiration and i want you to know that i love you and will always do.\n" +
+                "sent from heaven you are and on earth an angel\n" +
+                "forever yours till eternity!\n");
+
+    }
+
+
+
+
+}
